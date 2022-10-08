295d9571eb3e7f7a8b1a7f2e1fded40e2a0ae335
raypratidina <raypz642@gmail.com>
2022-10-07 17:25:33 -0700

    menambahkan static keyword

---
+++ oop/Static keyword/static.php
@@ -0,0 +1,23 @@
+<?php
+
+class Contoh {
+    public static $angka = 1;
+
+    public function halo() {
+        return "halo " . self::$angka++ . " kali . <br>";
+    }
+}
+
+$obj = new contoh();
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
+
+echo "<hr>" ;
+
+$obj2 = new contoh();
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
+
+?>
