1d96f95ef383523b9518004c4080c25f66a36de4
raypratidina <raypz642@gmail.com>
2022-10-14 15:08:54 -0700

    mvc

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_2022_b_203040099/mvc/public');
+
+//Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2022');
